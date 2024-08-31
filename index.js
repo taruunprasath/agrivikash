964ac18a192534c08f1703d37a60fb48553074e8
Taruun Prasath <taruunprasathofficial@gmail.com>
2024-08-31 18:12:23 +0530

    backend

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const stockRoutes = require('../backend/Routes/StockRouter');
 const userRoutes = require('../backend/Routes/UserRouter');
 const cors = require('cors');   
 
-mongoose.connect('mongodb+srv://taruunprasathgs2022eee:K83QmHmXHzs2DfJI@cluster-taruun.skzkrkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-Taruun/agrivikash').then(()=>{
+mongoose.connect('mongodb+srv://taruunprasathgs2022eee:K83QmHmXHzs2DfJI@cluster-taruun.skzkrkf.mongodb.net/agrivikash?retryWrites=true&w=majority&appName=Cluster-Taruun/agrivikash')
+.then(() => {
     console.log("Connected successfully");
-})
+});
+
 app.use(express.json());
 app.use(cors());
 
