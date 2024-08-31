beaa5282f9c8309a259998af20dee64150232574
Taruun Prasath <taruunprasathofficial@gmail.com>
2024-08-31 17:43:15 +0530

    backend

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 const stockRoutes = require('../backend/Routes/StockRouter');
 const userRoutes = require('../backend/Routes/UserRouter');
+const cors = require('cors');   
 
 mongoose.connect('mongodb://127.0.0.1:27017/agrivikash').then(()=>{
     console.log("Connected successfully");
 })
 app.use(express.json());
+app.use(cors());
+
 app.use('/stocks',stockRoutes);
 app.use('/user',userRoutes);
 
