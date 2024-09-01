b81fe4b3e6d4a6d792979df40d5d1717b4fa6ca7
Taruun Prasath <taruunprasathofficial@gmail.com>
2024-09-01 18:33:55 +0530

    added

--- Model/StockModel.js
+++ Model/StockModel.js
@@ -4,7 +4,7 @@ const stockSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'User', 
-        required: true
+        required: false
     },
     name: {
         type: String,
